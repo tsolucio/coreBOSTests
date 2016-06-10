5afcfbeadfdece7c54bc413a036a4730982ae511
Joe Bordes <joe@tsolucio.com>
2016-06-10 13:58:03 +0200

    (Permissions) isPermitted stub file to start work

---
+++ include/utils/UserInfoUtil_isPermittedTest.php
@@ -0,0 +1,38 @@
+<?php
+/*************************************************************************************************
+ * Copyright 2016 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Tests.
+ * The MIT License (MIT)
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *************************************************************************************************/
+
+/**
+ * Test the coreBOS Permission system via isPermitted function and RAC Business Rules
+ */
+class testUserInfoUtil_isPermittedTest extends PHPUnit_Framework_TestCase {
+
+	/****
+	 * TEST Users
+	 ****/
+	var $usrtestdmy = 5;
+	var $usrtestmdy = 6;
+	var $usrtestymd = 7;
+	var $usrtesttz = 10;
+	var $usrnocreate = 11;
+	var $usrtestmcurrency = 12;
+
+
+}
+?>
