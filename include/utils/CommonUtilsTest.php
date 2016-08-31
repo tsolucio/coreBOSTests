cde58a0a0847538e3ff70058fd73292ff7da1a9a
Joe Bordes <joe@tsolucio.com>
2016-08-31 16:33:15 +0200

    (Currency) partial tests for Common Utils: testing currency symbol position change

---
+++ include/utils/CommonUtilsTest.php
@@ -0,0 +1,59 @@
+<?php
+/*************************************************************************************************
+ * Copyright 2016 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Tests.
+ * The MIT License (MIT)
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *************************************************************************************************/
+
+class testCommonUtils extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Method testgetCurrencyName
+	 * @test
+	 */
+	public function testgetCurrencyName() {
+		$actual = getCurrencyName(1,false);
+		$this->assertEquals('Euro', $actual,"currency name 1 no symbol");
+		$actual = getCurrencyName(1,true);
+		$this->assertEquals('Euro : &euro;', $actual,"currency name 1 symbol");
+		$actual = getCurrencyName(2,false);
+		$this->assertEquals('USA, Dollars', $actual,"currency name 2 no symbol");
+		$actual = getCurrencyName(2,true);
+		$this->assertEquals('USA, Dollars : $', $actual,"currency name 2 symbol");
+	}
+
+	/**
+	 * Method testgetCurrencyName
+	 * @test
+	 */
+	public function testgetCurrencySymbolandCRate() {
+		$actual = getCurrencySymbolandCRate(1);
+		$expected = array(
+			'rate' => '1.0',
+			'symbol' => '&euro;',
+			'position' => '1.0$',
+		);
+		$this->assertEquals($expected, $actual,"currency symrate 1");
+		$actual = getCurrencySymbolandCRate(2);
+		$expected = array(
+			'rate' => '1.10',
+			'symbol' => '$',
+			'position' => '$1.0',
+		);
+		$this->assertEquals($expected, $actual,"currency symrate 2");
+	}
+
+}
