8918099171a0b2e61b74f08304f79f58ea86a164
Joe Bordes <joe@tsolucio.com>
2017-07-05 17:24:12 +0200

    (SimpleTemplate) inline workflow functions combinations

--- modules/com_vtiger_workflow/VTSimpleTemplate.php
+++ modules/com_vtiger_workflow/VTSimpleTemplate.php
@@ -45,10 +45,98 @@ class VTSimpleTemplateTest extends PHPUnit_Framework_TestCase {
 		$actual = $ct->render($entityCache, $entityId);
 		$this->assertEquals($expected, $actual,'User variables');
 		// Member of
-		$ct = new VTSimpleTemplate('$(account_id : (Accounts) accountname)​​​​​​​');
-		$expected = 'Rowley Schlimgen Inc​​​​​​​';
+		$ct = new VTSimpleTemplate('$(account_id : (Accounts) accountname)');
+		$expected = 'Rowley Schlimgen Inc';
 		$actual = $ct->render($entityCache, $entityId);
 		$this->assertEquals($expected, $actual,'Member of variables');
+		// Teardown
+		$util->revertUser();
+	}
+
+	/**
+	 * Method testRenderMultiline
+	 * @test
+	 */
+	public function testRenderMultiline() {
+		// Setup
+		$entityId = '13x5142';
+		$util = new VTWorkflowUtils();
+		$adminUser = $util->adminUser();
+		$entityCache = new VTEntityCache($adminUser);
+		// Potenial Email
+		$ct = new VTSimpleTemplate('<p>An Potential has been assigned to you on vtigerCRM<br />
+Details of Potential are :<br />
+<br />
+Potential No:<b>$potential_no</b><br />
+Potential Name:<b>$potentialname</b><br />
+Amount:<b>$amount</b><br />
+Expected Close Date:<b>$closingdate</b><br />
+Type:<b>$opportunity_type</b><br />
+<br />
+$(related_to : (Accounts) accountname)</p>
+
+<p>$(related_to : (Contacts) firstname)</p>
+
+<p><br />
+<br />
+Thank You<br />
+Admin</p>');
+		$expected = '<p>An Potential has been assigned to you on vtigerCRM<br />
+Details of Potential are :<br />
+<br />
+Potential No:<b>POT5</b><br />
+Potential Name:<b>Non Magna Industries</b><br />
+Amount:<b>80.786</b><br />
+Expected Close Date:<b>06-10-2016</b><br />
+Type:<b>--None--</b><br />
+<br />
+</p>
+
+<p>Sol</p>
+
+<p><br />
+<br />
+Thank You<br />
+Admin</p>';
+		$actual = $ct->render($entityCache, $entityId);
+		$this->assertEquals($expected, $actual,'Potential Email');
+		// Teardown
+		$util->revertUser();
+	}
+
+	/**
+	 * Method testRenderCalendar
+	 * @test
+	 */
+	public function testRenderCalendar() {
+		// Setup
+		$entityId = '41x27287';
+		$util = new VTWorkflowUtils();
+		$adminUser = $util->adminUser();
+		$entityCache = new VTEntityCache($adminUser);
+		// Constant string.
+		$ct = new VTSimpleTemplate('$(assigned_user_id : (Users) first_name) $(assigned_user_id : (Users) last_name),
+Activity Notification Details:
+Subject : $subject
+Start date and time : $date_start $time_start ($(general : (__VtigerMeta__) dbtimezone))
+End date and time : $due_date $time_end ($(general : (__VtigerMeta__) dbtimezone))
+Status : $eventstatus
+Priority : $taskpriority
+Related To : $(rel_id : (Leads) lastname)$(rel_id : (Leads) firstname)$(rel_id : (Accounts) accountname)$(rel_id : (Potentials) potentialname)$(rel_id : (HelpDesk) ticket_title)
+Contacts List : $(cto_id : (Contacts) lastname) $(cto_id : (Contacts) firstname)
+Location : $location');
+		$expected = ' Administrator,
+Activity Notification Details:
+Subject : test01m
+Start date and time : 2017-07-04 20:48:00 (UTC)
+End date and time : 2017-07-04 20:48:00 (UTC)
+Status : Planned
+Priority : High
+Related To : Natoque Penatibus Associates
+Contacts List : Hirpara Felix
+Location : ';
+		$actual = $ct->render($entityCache, $entityId);
+		$this->assertEquals($expected, $actual, 'Render Calendar email body');
 		// Teardown
 		$util->revertUser();
 	}
@@ -80,10 +168,14 @@ class VTSimpleTemplateTest extends PHPUnit_Framework_TestCase {
 		$actual = $ct->render($entityCache, $entityId);
 		$this->assertEquals($expected, $actual,'Record ID');
 		// Comments
-		$ct = new VTSimpleTemplate('$(general : (__VtigerMeta__) comments)​​​​​​​');
-		$expected = '<div class="comments"><div class="commentdetails"><ul class="commentfields"><li class="commentcreator"> Administrator</li><li class="commentdate">2015-10-06 11:31:58</li><li class="commentcomment">turpis nec mauris blandit mattis. Cras eget nisi dictum augue malesuada malesuada. Integer id magna et ipsum cursus vestibulum. Mauris magna. Duis dignissim tempor arcu. Vestibulum ut eros non enim commodo hendrerit. Donec porttitor tellus non magna. Nam ligula elit, pretium et, rutrum non, hendrerit id, ante. Nunc mauris sapien, cursus in, hendrerit consectetuer, cursus et, magna. Praesent interdum ligula eu enim. Etiam imperdiet dictum magna. Ut tincidunt orci quis lectus. Nullam suscipit, est ac facilisis facilisis, magna tellus faucibus leo, in lobortis tellus justo sit amet nulla. Donec non justo. Proin non massa non ante bibendum ullamcorper. Duis cursus, diam at pretium aliquet, metus urna convallis erat, eget tincidunt dui augue eu tellus. Phasellus elit pede, malesuada vel, venenatis vel, faucibus id, libero. Donec consectetuer mauris id sapien. Cras dolor dolor, tempus non, lacinia at, iaculis quis, pede.</li></ul></div><div class="commentdetails"><ul class="commentfields"><li class="commentcreator"> Administrator</li><li class="commentdate">2015-12-15 21:18:07</li><li class="commentcomment">nec metus facilisis lorem tristique aliquet. Phasellus fermentum convallis ligula. Donec luctus aliquet odio. Etiam ligula tortor, dictum eu, placerat eget, venenatis a, magna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam laoreet, libero et tristique pellentesque, tellus sem mollis dui, in sodales elit erat vitae risus. Duis a mi fringilla mi lacinia mattis. Integer eu lacus. Quisque imperdiet, erat nonummy ultricies ornare, elit elit fermentum risus, at fringilla purus mauris a nunc. In at pede. Cras vulputate velit eu sem. Pellentesque ut ipsum ac mi eleifend egestas. Sed pharetra, felis eget varius ultrices, mauris ipsum porta elit, a feugiat tellus lorem eu metus. In lorem. Donec elementum, lorem ut aliquam iaculis, lacus pede sagittis augue, eu tempor</li></ul></div></div>​​​​​​​';
+		$ct = new VTSimpleTemplate('$(general : (__VtigerMeta__) comments)');
+		$expected = '<div class="comments"><div class="commentdetails"><ul class="commentfields"><li class="commentcreator"> Administrator</li><li class="commentdate">2015-10-06 11:31:58</li><li class="commentcomment">turpis nec mauris blandit mattis. Cras eget nisi dictum augue malesuada malesuada. Integer id magna et ipsum cursus vestibulum. Mauris magna. Duis dignissim tempor arcu. Vestibulum ut eros non enim commodo hendrerit. Donec porttitor tellus non magna. Nam ligula elit, pretium et, rutrum non, hendrerit id, ante. Nunc mauris sapien, cursus in, hendrerit consectetuer, cursus et, magna. Praesent interdum ligula eu enim. Etiam imperdiet dictum magna. Ut tincidunt orci quis lectus. Nullam suscipit, est ac facilisis facilisis, magna tellus faucibus leo, in lobortis tellus justo sit amet nulla. Donec non justo. Proin non massa non ante bibendum ullamcorper. Duis cursus, diam at pretium aliquet, metus urna convallis erat, eget tincidunt dui augue eu tellus. Phasellus elit pede, malesuada vel, venenatis vel, faucibus id, libero. Donec consectetuer mauris id sapien. Cras dolor dolor, tempus non, lacinia at, iaculis quis, pede.</li></ul></div><div class="commentdetails"><ul class="commentfields"><li class="commentcreator"> Administrator</li><li class="commentdate">2015-12-15 21:18:07</li><li class="commentcomment">nec metus facilisis lorem tristique aliquet. Phasellus fermentum convallis ligula. Donec luctus aliquet odio. Etiam ligula tortor, dictum eu, placerat eget, venenatis a, magna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam laoreet, libero et tristique pellentesque, tellus sem mollis dui, in sodales elit erat vitae risus. Duis a mi fringilla mi lacinia mattis. Integer eu lacus. Quisque imperdiet, erat nonummy ultricies ornare, elit elit fermentum risus, at fringilla purus mauris a nunc. In at pede. Cras vulputate velit eu sem. Pellentesque ut ipsum ac mi eleifend egestas. Sed pharetra, felis eget varius ultrices, mauris ipsum porta elit, a feugiat tellus lorem eu metus. In lorem. Donec elementum, lorem ut aliquam iaculis, lacus pede sagittis augue, eu tempor</li></ul></div></div>';
 		$actual = $ct->render($entityCache, $entityId);
 		$this->assertEquals($expected, $actual,'Comments');
+		$ct = new VTSimpleTemplate('$(general : (__VtigerMeta__) comments_1d_text_comment)');
+		$expected = 'nec metus facilisis lorem tristique aliquet. Phasellus fermentum convallis ligula. Donec luctus aliquet odio. Etiam ligula tortor, dictum eu, placerat eget, venenatis a, magna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam laoreet, libero et tristique pellentesque, tellus sem mollis dui, in sodales elit erat vitae risus. Duis a mi fringilla mi lacinia mattis. Integer eu lacus. Quisque imperdiet, erat nonummy ultricies ornare, elit elit fermentum risus, at fringilla purus mauris a nunc. In at pede. Cras vulputate velit eu sem. Pellentesque ut ipsum ac mi eleifend egestas. Sed pharetra, felis eget varius ultrices, mauris ipsum porta elit, a feugiat tellus lorem eu metus. In lorem. Donec elementum, lorem ut aliquam iaculis, lacus pede sagittis augue, eu tempor';
+		$actual = $ct->render($entityCache, $entityId);
+		$this->assertEquals($expected, $actual,'Comments 1d_text_comment');
 		// Teardown
 		$util->revertUser();
 	}
@@ -99,32 +191,52 @@ class VTSimpleTemplateTest extends PHPUnit_Framework_TestCase {
 		$adminUser = $util->adminUser();
 		$entityCache = new VTEntityCache($adminUser);
 		// Contact first name uppercase
-		$ct = new VTSimpleTemplate('$(general : (__WorkflowFunction__) uppercase(firstname ))');
+		$ct = new VTSimpleTemplate('$(general : (__WorkflowFunction__) uppercase(firstname ) ) ');
 		$actual = $ct->render($entityCache, $entityId);
 		$this->assertEquals('CORAZON', $actual, 'uppercase(firstname )');
 		// uppercase string
-		$ct = new VTSimpleTemplate('$(general : (__WorkflowFunction__) '."uppercase('firstname' ))");
+		$ct = new VTSimpleTemplate('$(general : (__WorkflowFunction__) '."uppercase('firstname' ) ) ");
 		$actual = $ct->render($entityCache, $entityId);
 		$this->assertEquals('FIRSTNAME', $actual,'uppercase string');
 		// Formatted date
-		$ct = new VTSimpleTemplate('$(general : (__WorkflowFunction__) '."format_date(get_date('today'),'Ymd'))");
+		$ct = new VTSimpleTemplate('$(general : (__WorkflowFunction__) '."format_date(get_date('today'),'Ymd') ) ");
 		$actual = $ct->render($entityCache, $entityId);
 		$this->assertEquals(date('Ymd'), $actual,'Formatted date');
 		// concat related info
-		$ct = new VTSimpleTemplate('$(general : (__WorkflowFunction__) concat($(account_id : (Accounts) accountname),email ))');
+		$ct = new VTSimpleTemplate('$(general : (__WorkflowFunction__) concat($(account_id : (Accounts) accountname),email ) ) ');
 		$expected = 'Spieker Propertiescgrafenstein@gmail.com';
 		$actual = $ct->render($entityCache, $entityId);
 		$this->assertEquals($expected, $actual,'concat related info');
 		// concat related info with space
-		$ct = new VTSimpleTemplate('$(general : (__WorkflowFunction__) concat($(account_id : (Accounts) accountname),'."' - ', email ))");
+		$ct = new VTSimpleTemplate('$(general : (__WorkflowFunction__) concat($(account_id : (Accounts) accountname),'."' - ', email ) ) ");
 		$expected = 'Spieker Properties - cgrafenstein@gmail.com';
 		$actual = $ct->render($entityCache, $entityId);
 		$this->assertEquals($expected, $actual,'concat related info');
 		// concat related info with space
-		$ct = new VTSimpleTemplate('$(general : (__WorkflowFunction__) '."getCurrentUserName('full'))");
+		$ct = new VTSimpleTemplate('$(general : (__WorkflowFunction__) '."getCurrentUserName('full') ) ");
 		$expected = 'Administrator';
 		$actual = $ct->render($entityCache, $entityId);
 		$this->assertEquals($expected, $actual,'get full user name');
+		// accountname Contact first name uppercase
+		$ct = new VTSimpleTemplate('$(account_id : (Accounts) accountname) $(general : (__WorkflowFunction__) uppercase(firstname ) ) ');
+		$actual = $ct->render($entityCache, $entityId);
+		$this->assertEquals('Spieker Properties CORAZON', $actual, 'uppercase(firstname )');
+		// Contact first name uppercase + accountname
+		$ct = new VTSimpleTemplate('$(general : (__WorkflowFunction__) uppercase(firstname ) )  $(account_id : (Accounts) accountname)');
+		$actual = $ct->render($entityCache, $entityId);
+		$this->assertEquals('CORAZON Spieker Properties', $actual, 'uppercase(firstname )');
+		// uppercase string + accountname
+		$ct = new VTSimpleTemplate('$(general : (__WorkflowFunction__) '."uppercase('firstname' ) )  $(account_id : (Accounts) accountname)");
+		$actual = $ct->render($entityCache, $entityId);
+		$this->assertEquals('FIRSTNAME Spieker Properties', $actual,'uppercase string');
+		// Contact first name uppercase + accountname
+		$ct = new VTSimpleTemplate('$(general : (__WorkflowFunction__) uppercase(firstname) )  $(account_id : (Accounts) accountname)');
+		$actual = $ct->render($entityCache, $entityId);
+		$this->assertEquals('CORAZON Spieker Properties', $actual, 'uppercase(firstname )');
+		// uppercase string + accountname
+		$ct = new VTSimpleTemplate('$(general : (__WorkflowFunction__) '."uppercase('firstname') )  $(account_id : (Accounts) accountname)");
+		$actual = $ct->render($entityCache, $entityId);
+		$this->assertEquals('FIRSTNAME Spieker Properties', $actual,'uppercase string');
 		// Teardown
 		$util->revertUser();
 	}
