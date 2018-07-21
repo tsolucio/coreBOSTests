cf4e8c229d59fffe97b08e94c99ab40f1099f64b
Joe Bordes <joe@tsolucio.com>
2018-07-21 13:02:34 +0200

    fix(Webservice:getRelatedModules)

--- include/Webservices/getRelatedModulesTest.php
+++ include/Webservices/getRelatedModulesTest.php
@@ -33,6 +33,7 @@ class getRelatedModulesTest extends TestCase {
 		$expected = array(
 			'Contacts' => array(
 				'related_tabid' => '4',
+				'related_module' => 'Contacts',
 				'label' => 'Contacts',
 				'labeli18n' => 'Contacts',
 				'actions' => 'add',
@@ -40,27 +41,31 @@ class getRelatedModulesTest extends TestCase {
 			),
 			'Potentials' => array(
 				'related_tabid' => '2',
+				'related_module' => 'Potentials',
 				'label' => 'Potentials',
-				'labeli18n' => 'Potentials',
+				'labeli18n' => 'Opportunities',
 				'actions' => 'add',
 				'relationId' => '2',
 			),
 			'Quotes' => array(
 				'related_tabid' => '20',
+				'related_module' => 'Quotes',
 				'label' => 'Quotes',
 				'labeli18n' => 'Quotes',
 				'actions' => 'add',
 				'relationId' => '3',
 			),
 			'Sales Order' => array(
-				'actions' => 'add',
 				'related_tabid' => '22',
+				'related_module' => 'SalesOrder',
 				'label' => 'Sales Order',
 				'labeli18n' => 'Sales Order',
+				'actions' => 'add',
 				'relationId' => '4',
 			),
 			'Invoice' => array(
 				'related_tabid' => '23',
+				'related_module' => 'Invoice',
 				'label' => 'Invoice',
 				'labeli18n' => 'Invoice',
 				'actions' => 'add',
@@ -68,6 +73,7 @@ class getRelatedModulesTest extends TestCase {
 			),
 			'Activities' => array(
 				'related_tabid' => '63',
+				'related_module' => 'cbCalendar',
 				'label' => 'Activities',
 				'labeli18n' => 'Activities',
 				'actions' => 'add',
@@ -75,13 +81,15 @@ class getRelatedModulesTest extends TestCase {
 			),
 			'Emails' => array(
 				'related_tabid' => '10',
+				'related_module' => 'Emails',
 				'label' => 'Emails',
-				'labeli18n' => 'Emails',
+				'labeli18n' => 'Email',
 				'actions' => 'add',
 				'relationId' => '7',
 			),
 			'Documents' => array(
 				'related_tabid' => '8',
+				'related_module' => 'Documents',
 				'label' => 'Documents',
 				'labeli18n' => 'Documents',
 				'actions' => 'add,select',
@@ -89,13 +97,15 @@ class getRelatedModulesTest extends TestCase {
 			),
 			'HelpDesk' => array(
 				'related_tabid' => '13',
+				'related_module' => 'HelpDesk',
 				'label' => 'HelpDesk',
-				'labeli18n' => 'HelpDesk',
+				'labeli18n' => 'Support Tickets',
 				'actions' => 'add',
 				'relationId' => '10',
 			),
 			'Products' => array(
 				'related_tabid' => '14',
+				'related_module' => 'Products',
 				'label' => 'Products',
 				'labeli18n' => 'Products',
 				'actions' => 'select',
@@ -103,6 +113,7 @@ class getRelatedModulesTest extends TestCase {
 			),
 			'Campaigns' => array(
 				'related_tabid' => '26',
+				'related_module' => 'Campaigns',
 				'label' => 'Campaigns',
 				'labeli18n' => 'Campaigns',
 				'actions' => 'select',
@@ -110,6 +121,7 @@ class getRelatedModulesTest extends TestCase {
 			),
 			'Service Contracts' => array(
 				'related_tabid' => '37',
+				'related_module' => 'ServiceContracts',
 				'label' => 'Service Contracts',
 				'labeli18n' => 'Service Contracts',
 				'actions' => 'ADD',
@@ -117,6 +129,7 @@ class getRelatedModulesTest extends TestCase {
 			),
 			'Services' => array(
 				'related_tabid' => '38',
+				'related_module' => 'Services',
 				'label' => 'Services',
 				'labeli18n' => 'Services',
 				'actions' => 'SELECT',
@@ -124,13 +137,15 @@ class getRelatedModulesTest extends TestCase {
 			),
 			'CobroPago' => array(
 				'related_tabid' => '42',
+				'related_module' => 'CobroPago',
 				'label' => 'CobroPago',
-				'labeli18n' => 'CobroPago',
+				'labeli18n' => 'Payments',
 				'actions' => 'ADD',
 				'relationId' => '116',
 			),
 			'Assets' => array(
 				'related_tabid' => '43',
+				'related_module' => 'Assets',
 				'label' => 'Assets',
 				'labeli18n' => 'Assets',
 				'actions' => 'ADD',
@@ -138,6 +153,7 @@ class getRelatedModulesTest extends TestCase {
 			),
 			'Projects' => array(
 				'related_tabid' => '50',
+				'related_module' => 'Project',
 				'label' => 'Projects',
 				'labeli18n' => 'Projects',
 				'actions' => 'ADD',
@@ -145,10 +161,27 @@ class getRelatedModulesTest extends TestCase {
 			),
 			'InventoryDetails' => array(
 				'related_tabid' => '57',
+				'related_module' => 'InventoryDetails',
 				'label' => 'InventoryDetails',
-				'labeli18n' => 'InventoryDetails',
+				'labeli18n' => 'Inventory Details',
 				'actions' => '',
 				'relationId' => '155',
+			),
+			'cbSurveyDone' => array(
+				'related_tabid' => '67',
+				'related_module' => 'cbSurveyDone',
+				'label' => 'cbSurveyDone',
+				'labeli18n' => 'Surveys Done',
+				'actions' => 'ADD',
+				'relationId' => '177',
+			),
+			'cbSurveyAnswer' => array(
+				'related_tabid' => '68',
+				'related_module' => 'cbSurveyAnswer',
+				'label' => 'cbSurveyAnswer',
+				'labeli18n' => 'Surveys Answer',
+				'actions' => 'ADD',
+				'relationId' => '181',
 			)
 		);
 		$this->assertEquals($expected, $actual, 'testgetRelatedModulesInfomation get accounts');
@@ -157,13 +190,15 @@ class getRelatedModulesTest extends TestCase {
 		$expected = array(
 			'HelpDesk' => array(
 				'related_tabid' => '13',
+				'related_module' => 'HelpDesk',
 				'label' => 'HelpDesk',
-				'labeli18n' => 'HelpDesk',
+				'labeli18n' => 'Support Tickets',
 				'actions' => 'ADD,SELECT',
-				'relationId' => '127',
+				'relationId' => '126',
 			),
-			'Potentials' => array(
+			'Documents' => array(
 				'related_tabid' => '8',
+				'related_module' => 'Documents',
 				'label' => 'Documents',
 				'labeli18n' => 'Documents',
 				'actions' => 'ADD,SELECT',
