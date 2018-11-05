9da824c9fed8a51e5e0b7d77c8698549a9d67027
Kliv Shapllo <k.shapllo@studiosynthesis.biz>
2018-11-05 18:02:31 +0100

    fix(BusinessAction) Code formatting fixes

--- vtlib/Vtiger/BusinessActionsTest.php
+++ vtlib/Vtiger/BusinessActionsTest.php
@@ -22,10 +22,10 @@ use PHPUnit\Framework\TestCase;
 
 class BusinessActionsTest extends TestCase {
 
-    /**
-     * Method testgetAllByType
-     * @test
-     */
+	/**
+	 * Method testgetAllByType
+	 * @test
+	 */
 	public function testgetAllByType() {
 		$customlink_params = array('MODULE'=>'CobroPago', 'RECORD'=>14297, 'ACTION'=>'DetailView');
 		$actual = Vtiger_Link::getAllByType(getTabid('CobroPago'), array('DETAILVIEWBASIC', 'DETAILVIEW', 'DETAILVIEWWIDGET'), $customlink_params);
@@ -71,146 +71,145 @@ class BusinessActionsTest extends TestCase {
 		$this->assertEquals($expected, $actual);
 	}
 
-    /**
-     * Method testaddLink
-     * @test
-     */
-    public function testaddLink(){
-        $module_contacts = Vtiger_Module::getInstance('Contacts');
-        $expectedLinks = $module_contacts->getLinks();
+	/**
+	 * Method testaddLink
+	 * @test
+	 */
+	public function testaddLink() {
+		$module_contacts = Vtiger_Module::getInstance('Contacts');
+		$expectedLinks = $module_contacts->getLinks();
 
-        // Link attributes
-        $tabid = getTabid('Contacts');
-        $linktype = 'LISTVIEWBASIC';
-        $linklabel = 'LBL_SELECT_ALL';
-        $linkurl = 'toggleSelectAllEntries_ListView();';
-        $linkicon = '';
-        $sequence = '0';
-        $handler_path = '';
-        $handler_class = '';
-        $handler = '';
-        $onlyonmymodule = '0';
+		// Link attributes
+		$tabid = getTabid('Contacts');
+		$linktype = 'LISTVIEWBASIC';
+		$linklabel = 'LBL_SELECT_ALL';
+		$linkurl = 'toggleSelectAllEntries_ListView();';
+		$linkicon = '';
+		$sequence = '0';
+		$handler_path = '';
+		$handler_class = '';
+		$handler = '';
+		$onlyonmymodule = '0';
 
-        // Adding link
-        $module_contacts->addLink($linktype, $linklabel, $linkurl);
-        $actualLinks = $module_contacts->getLinks();
+		// Adding link
+		$module_contacts->addLink($linktype, $linklabel, $linkurl);
+		$actualLinks = $module_contacts->getLinks();
 
-        // Getting linkid
-        $lastLink = end($actualLinks);
-        reset($actualLinks);
-        $linkid = $lastLink->linkid;
+		// Getting linkid
+		$lastLink = end($actualLinks);
+		reset($actualLinks);
+		$linkid = $lastLink->linkid;
 
-        // Link object
-        $link = new Vtiger_Link();
-        $link->tabid = $tabid;
-        $link->linkid = $linkid;
-        $link->linktype = $linktype;
-        $link->linklabel = $linklabel;
-        $link->linkurl = $linkurl;
-        $link->linkicon = $linkicon;
-        $link->sequence = $sequence;
-        $link->handler_path = $handler_path;
-        $link->handler_class = $handler_class;
-        $link->handler = $handler;
-        $link->onlyonmymodule = $onlyonmymodule;
+		// Link object
+		$link = new Vtiger_Link();
+		$link->tabid = $tabid;
+		$link->linkid = $linkid;
+		$link->linktype = $linktype;
+		$link->linklabel = $linklabel;
+		$link->linkurl = $linkurl;
+		$link->linkicon = $linkicon;
+		$link->sequence = $sequence;
+		$link->handler_path = $handler_path;
+		$link->handler_class = $handler_class;
+		$link->handler = $handler;
+		$link->onlyonmymodule = $onlyonmymodule;
 
-        // Adding link object to expectedLinks
-        $expectedLinks["LISTVIEWBASIC"] = $link;
+		// Adding link object to expectedLinks
+		$expectedLinks["LISTVIEWBASIC"] = $link;
 
-        // Delete created link
-        Vtiger_Link::deleteLink($tabid, $linktype, $linklabel);
+		// Delete created link
+		Vtiger_Link::deleteLink($tabid, $linktype, $linklabel);
 
-        $this->assertEquals($expectedLinks, $actualLinks);
-    }
+		$this->assertEquals($expectedLinks, $actualLinks);
+	}
 
-    /**
-     * Method testupdateLink
-     * @test
-     */
-    public function testupdateLink(){
-        // Module links
-        $module_contacts = Vtiger_Module::getInstance('Contacts');
-        $links = $module_contacts->getLinks();
+	/**
+	 * Method testupdateLink
+	 * @test
+	 */
+	public function testupdateLink() {
+		// Module links
+		$module_contacts = Vtiger_Module::getInstance('Contacts');
+		$links = $module_contacts->getLinks();
 
-        // Last link
-        $lastLink = end($links);
-        reset($links);
+		// Last link
+		$lastLink = end($links);
+		reset($links);
 
-        $expectedLinkLabel = "Send Email";
+		$expectedLinkLabel = "Send Email";
 
-        // Update module last link
-        Vtiger_Link::updateLink($lastLink->tabid, $lastLink->linkid, array('linklabel' => $expectedLinkLabel));
+		// Update module last link
+		Vtiger_Link::updateLink($lastLink->tabid, $lastLink->linkid, array('linklabel' => $expectedLinkLabel));
 
-        // Updated module links
-        $newLinks =$module_contacts->getLinks();
-        $newLastLink = end($newLinks);
-        reset($newLinks);
+		// Updated module links
+		$newLinks =$module_contacts->getLinks();
+		$newLastLink = end($newLinks);
+		reset($newLinks);
 
-        $actualLinkLabel = $newLastLink->linklabel;
+		$actualLinkLabel = $newLastLink->linklabel;
 
-        // Restore module link value
-        Vtiger_Link::updateLink($lastLink->tabid, $lastLink->linkid, array('linklabel' => $lastLink->linklabel));
+		// Restore module link value
+		Vtiger_Link::updateLink($lastLink->tabid, $lastLink->linkid, array('linklabel' => $lastLink->linklabel));
 
-        $this->assertEquals($expectedLinkLabel, $actualLinkLabel);
-    }
+		$this->assertEquals($expectedLinkLabel, $actualLinkLabel);
+	}
 
-    /**
-     * Method testdeleteLink
-     * @test
-     */
-    public function testdeleteLink(){
-        // Module links
-        $module_contacts = Vtiger_Module::getInstance('Contacts');
-        $actualLinks = $module_contacts->getLinks();
+	/**
+	 * Method testdeleteLink
+	 * @test
+	 */
+	public function testdeleteLink() {
+		// Module links
+		$module_contacts = Vtiger_Module::getInstance('Contacts');
+		$actualLinks = $module_contacts->getLinks();
 
-        // Last link
-        $lastLink = end($actualLinks);
-        reset($actualLinks);
+		// Last link
+		$lastLink = end($actualLinks);
+		reset($actualLinks);
 
-        // Remove last link
-        array_pop($actualLinks);
+		// Remove last link
+		array_pop($actualLinks);
 
-        // Delete last link
-        Vtiger_Link::deleteLink($lastLink->tabid, $lastLink->linktype, $lastLink->linklabel);
+		// Delete last link
+		Vtiger_Link::deleteLink($lastLink->tabid, $lastLink->linktype, $lastLink->linklabel);
 
-        // Remained links
-        $expectedLinks = $module_contacts->getLinks();
+		// Remained links
+		$expectedLinks = $module_contacts->getLinks();
 
-        // Restore link
-        $handlerInfo = array();
-        $handlerInfo['path'] = $lastLink->handler_path;
-        $handlerInfo['class'] = $lastLink->handler_class;
-        $handlerInfo['method'] = $lastLink->handler;
+		// Restore link
+		$handlerInfo = array();
+		$handlerInfo['path'] = $lastLink->handler_path;
+		$handlerInfo['class'] = $lastLink->handler_class;
+		$handlerInfo['method'] = $lastLink->handler;
 
-        $module_contacts->addLink($lastLink->linktype, $lastLink->linklabel, $lastLink->linkurl, $lastLink->icon, $lastLink->sequence, $handlerInfo, $lastLink->onlyonmymodule);
+		$module_contacts->addLink($lastLink->linktype, $lastLink->linklabel, $lastLink->linkurl, $lastLink->icon, $lastLink->sequence, $handlerInfo, $lastLink->onlyonmymodule);
 
-        $this->assertEquals($expectedLinks, $actualLinks);
-    }
+		$this->assertEquals($expectedLinks, $actualLinks);
+	}
 
-    /**
-     * Method testdeleteAll
-     * @test
-     */
-    public function testdeleteAll(){
-        // Module links
-        $module_contacts = Vtiger_Module::getInstance('Contacts');
-        $actualLinks = $module_contacts->getLinks();
+	/**
+	 * Method testdeleteAll
+	 * @test
+	 */
+	public function testdeleteAll() {
+		// Module links
+		$module_contacts = Vtiger_Module::getInstance('Contacts');
+		$actualLinks = $module_contacts->getLinks();
 
-        // Delete all module links
-        Vtiger_Link::deleteAll(getTabid('Contacts'));
+		// Delete all module links
+		Vtiger_Link::deleteAll(getTabid('Contacts'));
 
-        $expectedLinks = $module_contacts->getLinks();
+		$expectedLinks = $module_contacts->getLinks();
 
-        // Restore module links
-        foreach($actualLinks as $link){
-            $handlerInfo['path'] = $link->handler_path;
-            $handlerInfo['class'] = $link->handler_class;
-            $handlerInfo['method'] = $link->handler;
+		// Restore module links
+		foreach ($actualLinks as $link) {
+			$handlerInfo['path'] = $link->handler_path;
+			$handlerInfo['class'] = $link->handler_class;
+			$handlerInfo['method'] = $link->handler;
 
-            $module_contacts->addLink($link->linktype, $link->linklabel, $link->linkurl, $link->icon, (int)$link->sequence, $handlerInfo, $link->onlyonmymodule);
-        }
+			$module_contacts->addLink($link->linktype, $link->linklabel, $link->linkurl, $link->icon, (int)$link->sequence, $handlerInfo, $link->onlyonmymodule);
+		}
 
-        $this->assertEquals(0, count($expectedLinks));
-    }
-
+		$this->assertEquals(0, count($expectedLinks));
+	}
 }
