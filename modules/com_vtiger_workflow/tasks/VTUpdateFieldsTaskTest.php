28141a38f6988038522d995fd5f58e323d4da846
Joe Bordes <joe@tsolucio.com>
2019-02-16 20:07:15 +0100

    (workflow) additional assertions to validate data consistency in Update field task

--- modules/com_vtiger_workflow/tasks/VTUpdateFieldsTaskTest.php
+++ modules/com_vtiger_workflow/tasks/VTUpdateFieldsTaskTest.php
@@ -27,6 +27,9 @@ class VTUpdateFieldsTaskTest extends TestCase {
 	 */
 	public function testCorrectUpdate() {
 		global $adb, $current_user;
+		$preValues = CRMEntity::getInstance('InventoryDetails');
+		$preValues->retrieve_entity_info(2823, 'InventoryDetails');
+		unset($preValues->column_fields['modifiedtime']);
 		// Using Inventory Lines existing workflow update task we force a launch and check the update result
 		$taskId = 14; // line complete inventory details update task
 		$InventoryDetailsWSID = '36x';
@@ -49,6 +52,35 @@ class VTUpdateFieldsTaskTest extends TestCase {
 		$this->assertEquals($expected, $actual, 'Task update field');
 		// Teardown
 		$util->revertUser();
+		$postValues = CRMEntity::getInstance('InventoryDetails');
+		$postValues->retrieve_entity_info(2823, 'InventoryDetails');
+		unset($postValues->column_fields['modifiedtime']);
+		$this->assertEquals($preValues, $postValues, 'Task update record does not change');
+		// Now with normal user that has restricted access to some fields
+		$user = new Users();
+		$user->retrieveCurrentUserInfoFromFile(6); // testmdy
+		$holduser = $current_user;
+		$current_user = $user;
+		$preValues = CRMEntity::getInstance('InventoryDetails');
+		$preValues->retrieve_entity_info(2823, 'InventoryDetails');
+		unset($preValues->column_fields['modifiedtime']);
+		$adb->pquery('update vtiger_inventorydetails set line_completed=?, units_delivered_received=3, quantity=3 where inventorydetailsid=?', array('0',2823));
+		$tm = new VTTaskManager($adb);
+		$task = $tm->retrieveTask($taskId);
+		$this->assertInstanceOf(VTUpdateFieldsTask::class, $task, 'test retrieveTask');
+		list($moduleId, $crmId) = explode('x', $entityId);
+		$entity = new VTWorkflowEntity($current_user, $entityId);
+		$task->doTask($entity);
+		$rs = $adb->pquery('select line_completed from vtiger_inventorydetails where inventorydetailsid=?', array($crmId));
+		$actual = $adb->query_result($rs, 0, 0);
+		$expected = '1';
+		$this->assertEquals($expected, $actual, 'Task update field');
+		// Teardown
+		$postValues = CRMEntity::getInstance('InventoryDetails');
+		$postValues->retrieve_entity_info(2823, 'InventoryDetails');
+		unset($postValues->column_fields['modifiedtime']);
+		$this->assertEquals($preValues, $postValues, 'Task update record does not change');
+		$current_user = $holduser;
 	}
 
 	public function testCorrectUpdateAssignedTo() {
@@ -56,7 +88,10 @@ class VTUpdateFieldsTaskTest extends TestCase {
 
 		$taskId = 28; //  Update Assigned to  task id
 		$HelpDeskWSID = '17x';
-		$lastcrmid='34042';// the id of the last HelpDesk Record partially created due to the error on the next wf
+		$lastcrmid='2780';// the id of the last HelpDesk Record partially created due to the error on the next wf
+		$preValues = CRMEntity::getInstance('HelpDesk');
+		$preValues->retrieve_entity_info($lastcrmid, 'HelpDesk');
+		unset($preValues->column_fields['assigned_user_id'], $preValues->column_fields['modifiedtime']);
 		$adb->pquery('update vtiger_crmentity set smownerid=? where crmid=?', array('1',$lastcrmid));
 		$entityId = $HelpDeskWSID.$lastcrmid;
 		$util = new VTWorkflowUtils();
@@ -86,5 +121,34 @@ class VTUpdateFieldsTaskTest extends TestCase {
 		$this->assertEquals($expected, $actual, 'Task update field assigned_user_id');
 		// Teardown
 		$util->revertUser();
+		$postValues = CRMEntity::getInstance('HelpDesk');
+		$postValues->retrieve_entity_info($lastcrmid, 'HelpDesk');
+		unset($postValues->column_fields['assigned_user_id'], $postValues->column_fields['modifiedtime']);
+		$this->assertEquals($preValues, $postValues, 'HelpDesk update record does not change');
+		// Now with normal user
+		$taskId = 28; //  Update Assigned to  task id
+		$user = new Users();
+		$user->retrieveCurrentUserInfoFromFile(6); // testmdy
+		$holduser = $current_user;
+		$current_user = $user;
+		$adb->pquery('update vtiger_crmentity set smownerid=? where crmid=?', array('6', $lastcrmid));
+		$preValues = CRMEntity::getInstance('HelpDesk');
+		$preValues->retrieve_entity_info($lastcrmid, 'HelpDesk');
+		unset($preValues->column_fields['assigned_user_id'], $preValues->column_fields['modifiedtime']);
+		$tm = new VTTaskManager($adb);
+		$task = $tm->retrieveTask($taskId);
+		$this->assertInstanceOf(VTUpdateFieldsTask::class, $task, 'test retrieveTask');
+		list($moduleId, $crmId) = explode('x', $entityId);
+		$entity = new VTWorkflowEntity($current_user, $entityId);
+		$task->doTask($entity);
+		$actual=$entity->get('assigned_user_id');
+		$expected = '19x8';
+		$this->assertEquals($expected, $actual, 'Task update field assigned_user_id');
+		// Teardown
+		$adb->pquery('update vtiger_crmentity set smownerid=? where crmid=?', array('6', $lastcrmid));
+		$postValues = CRMEntity::getInstance('HelpDesk');
+		$postValues->retrieve_entity_info($lastcrmid, 'HelpDesk');
+		unset($postValues->column_fields['assigned_user_id'], $postValues->column_fields['modifiedtime']);
+		$this->assertEquals($preValues, $postValues, 'HelpDesk update record does not change');
 	}
 }
