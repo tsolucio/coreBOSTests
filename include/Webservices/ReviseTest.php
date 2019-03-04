8b1d5af90b4c390317933b4da375eca7708894db
Joe Bordes <joe@tsolucio.com>
2019-03-04 18:05:56 +0100

    (Revise) do not lose currency and taxes

--- include/Webservices/ReviseTest.php
+++ include/Webservices/ReviseTest.php
@@ -36,7 +36,7 @@ class WSReviseTest extends TestCase {
 	 * @test
 	 */
 	public function testReviseWithCheckboxes() {
-		global $current_user;
+		global $current_user, $adb;
 		$holduser = $current_user;
 		$user = new Users();
 		///////////
@@ -114,6 +114,131 @@ class WSReviseTest extends TestCase {
 		$expected['modifiedtime'] = $actual['modifiedtime'];
 		$expected['modifiedby'] = $actual['modifiedby'];
 		$this->assertEquals($expected, $actual, 'Test checkbox usrdota0x Correct');
+		$adb->pquery("update vtiger_contactsubdetails set otherphone='' where contactid=?", array(1150));
+		$adb->pquery("update vtiger_contactdetails set donotcall='0', emailoptout='0', reference='0', notify_owner='0' where contactid=?", array(1150));
+		/// end
+		$current_user = $holduser;
+	}
+
+	/**
+	 * Method testReviseMultiCurrencyTaxes
+	 * @test
+	 */
+	public function testReviseMultiCurrencyTaxes() {
+		global $current_user, $adb;
+		$holduser = $current_user;
+		$user = new Users();
+		$user->retrieveCurrentUserInfoFromFile($this->usrcomd0x); // testmdy
+		$current_user = $user;
+		$cbUserID = '19x'.$current_user->id;
+		///////////  Services
+		$ObjectValues = array(
+			'servicename' => 'Grocery store',
+			'service_no' => 'SER18',
+			'service_usageunit' => 'Incidents',
+			'discontinued' => '1',
+			'qty_per_unit' => '7.00',
+			'website' => '',
+			'servicecategory' => 'Transportation',
+			'assigned_user_id' => '19x5',
+			'sales_start_date' => '2015-10-22',
+			'sales_end_date' => '2016-04-27',
+			'start_date' => '2016-04-09',
+			'expiry_date' => '2016-06-30',
+			'createdtime' => '2015-06-19 07:09:51',
+			'modifiedtime' => '2019-03-03 21:40:36',
+			'modifiedby' => '19x1',
+			'created_user_id' => '19x1',
+			'unit_price' => '37.400000',
+			'commissionrate' => '5.000',
+			'taxclass' => '',
+			'cost_price' => '0.000000',
+			'divisible' => '0',
+			'cf_802' => '',
+			'description' => 'Phasellus at augue id ante dictum cursus. Nunc mauris elit, dictum eu, eleifend nec, malesuada ut, sem. Nulla interdum. Curabitur dictum. Phasellus in felis. Nulla tempor augue ac ipsum. Phasellus vitae mauris sit amet lorem semper auctor. Mauris vel turpis. Aliquam adipiscing lobortis risus. In mi pede, nonummy ut, molestie in, tempus eu, ligula. Aenean euismod mauris eu elit. Nulla facilisi. Sed neque. Sed eget lacus. Mauris non dui nec urna suscipit nonummy. Fusce fermentum fermentum arcu. Vestibulum ante ipsum',
+			'id' => '26x9727',
+		);
+		$prePdoPrices = getPriceDetailsForProduct(9727, 3.89, 'available_associated', 'Services');
+		$preTaxDetails = getTaxDetailsForProduct(9727, 'available_associated');
+		$updateValues = array(
+			'divisible' => '1',
+			'commissionrate' => 22,
+			'id' => '26x9727',
+		);
+		$_FILES = array();
+		$actual = vtws_revise($updateValues, $current_user);
+		$expected = $ObjectValues;
+		$expected['divisible'] = '1';
+		$expected['commissionrate'] = 22;
+		$expected['modifiedtime'] = $actual['modifiedtime'];
+		$expected['modifiedby'] = $actual['modifiedby'];
+		$this->assertEquals($expected, $actual, 'Test Services usrcomd0x Correct');
+		$postPdoPrices = getPriceDetailsForProduct(9727, 3.89, 'available_associated', 'Services');
+		$postTaxDetails = getTaxDetailsForProduct(9727, 'available_associated');
+		$this->assertEquals($prePdoPrices, $postPdoPrices, 'Test Services usrcomd0x Currency Correct');
+		$this->assertEquals($preTaxDetails, $postTaxDetails, 'Test Services usrcomd0x Taxes Correct');
+		$adb->pquery("update vtiger_service set divisible='0', commissionrate=5.0 where serviceid=?", array(9727));
+		///////////  Products
+		$ObjectValues = array(
+			'productname' => 'Leagoo Lead 3s Mobile Phone',
+			'product_no' => 'PRO18',
+			'discontinued' => '1',
+			'productcode' => '',
+			'sales_start_date' => '2015-07-10',
+			//'manufacturer' => 'AltvetPet Inc.', // this user does not have access to this field
+			//'productcategory' => 'Software', // this user does not have access to this field
+			'start_date' => '2012-12-31',
+			'sales_end_date' => '2024-08-03',
+			'expiry_date' => '2025-08-24',
+			'vendor_id' => '2x2363',
+			//'website' => '', // this user does not have access to this field
+			//'vendor_part_no' => 'áçèñtös', // this user does not have access to this field
+			'mfr_part_no' => '',
+			'productsheet' => '',
+			'serial_no' => '', // this user does not have access to this field
+			'createdtime' => '2015-04-06 17:00:58',
+			'glacct' => '302-Rental-Income',
+			'modifiedtime' => '2019-02-21 22:12:55',
+			'modifiedby' => '19x1',
+			'created_user_id' => '19x1',
+			'unit_price' => '223.000000',
+			'commissionrate' => '0.000',
+			'taxclass' => '',
+			'cost_price' => '200.000000',
+			'usageunit' => 'M',
+			'qty_per_unit' => '0.00',
+			'qtyinstock' => '6.000',
+			'reorderlevel' => '0',
+			'assigned_user_id' => '19x6',
+			'qtyindemand' => '0',
+			'divisible' => '0',
+			'imagename' => '',
+			'description' => 'Teneo, inquit, finem illi videri nihil dolere. Quonam modo? Sin te auctoritas commovebat, nobisne omnibus et Platoni ipsi nescio quem illum anteponebas? Cum praesertim illa perdiscere ludus esset.',
+			'id' => '14x2633',
+		);
+		$prePdoPrices = getPriceDetailsForProduct(2633, 3.89, 'available_associated', 'Products');
+		$preTaxDetails = getTaxDetailsForProduct(2633, 'available_associated');
+		$updateValues = array(
+			'productcode' => 'testthis',
+			'productsheet' => 'productsheet',
+			'serial_no' => '123456789',
+			'id' => '14x2633',
+		);
+		$_FILES = array();
+		$actual = vtws_revise($updateValues, $current_user);
+		$expected = $ObjectValues;
+		$expected['productcode'] = 'testthis';
+		$expected['productsheet'] = 'productsheet';
+		$expected['modifiedtime'] = $actual['modifiedtime'];
+		$expected['modifiedby'] = $actual['modifiedby'];
+		$this->assertTrue(!isset($actual['serial_no']), 'user does not have access to this field');
+		$actual['serial_no'] = ''; // this user does not have access to this field so it should not update
+		$this->assertEquals($expected, $actual, 'Test product usrcomd0x Correct');
+		$postPdoPrices = getPriceDetailsForProduct(2633, 3.89, 'available_associated', 'Products');
+		$postTaxDetails = getTaxDetailsForProduct(2633, 'available_associated');
+		$this->assertEquals($prePdoPrices, $postPdoPrices, 'Test product usrcomd0x Currency Correct');
+		$this->assertEquals($preTaxDetails, $postTaxDetails, 'Test product usrcomd0x Taxes Correct');
+		$adb->pquery("update vtiger_products set productcode='', productsheet='', serialno='' where productid=?", array(2633));
 		/// end
 		$current_user = $holduser;
 	}
