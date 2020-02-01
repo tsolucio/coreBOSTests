7dc6da6d4004d615af8fb688717ba151a70dbaa3
joebordes <joe@tsolucio.com>
2020-02-01 23:43:11 +0100

    e2e(Login) replicate selenium login tests with jest/puppeteer

---
+++ e2e/Access/login.test.js
@@ -0,0 +1,87 @@
+const puppeteer = require('puppeteer')
+const getUrlFromConfigFile = require('../../utils.js').getUrlFromConfigFile
+
+test('Admin login', async () => {
+	jest.setTimeout(30000);
+	const url = getUrlFromConfigFile();
+	if (!!url) {
+		// Uncomment this section to actually see the test happening
+		const browser = await puppeteer.launch({
+			// headless: false,
+			// slowMo: 80,
+			// args: ['--window-size=1920,1080'],
+			defaultViewport: null
+		})
+
+		const page = await browser.newPage()
+		await page.goto(url)
+		await page.type('input#username', 'admin')
+		await page.type('input#password', 'admin')
+		await page.click('input#Login')
+		//await page.click('input#Login').then(() => page.waitForNavigation({waitUntil: 'load'}));
+		//page.waitForNavigation({ waitUntil: 'load' })
+		await page.waitForSelector('#global-header');
+		const ptitle = await page.title()
+		expect(ptitle).toBe('Administrator - Home - coreBOS')
+		const btn = await page.$eval('button[title="CRM Settings"]', el => el)
+		expect(btn).toBeTruthy()
+		browser.close()
+	} else {
+		throw 'URL could not be determined from config file'
+	}
+})
+
+test('Non Admin login', async () => {
+	jest.setTimeout(30000);
+	const url = getUrlFromConfigFile();
+	if (!!url) {
+		// Uncomment this section to actually see the test happening
+		const browser = await puppeteer.launch({
+			// headless: false,
+			// slowMo: 80,
+			// args: ['--window-size=1920,1080'],
+			defaultViewport: null
+		})
+
+		const page = await browser.newPage()
+		await page.goto(url)
+		await page.type('input#username', 'testdmy')
+		await page.type('input#password', 'testdmy')
+		await page.click('input#Login')
+		//await page.click('input#Login').then(() => page.waitForNavigation({waitUntil: 'load'}));
+		//page.waitForNavigation({ waitUntil: 'load' })
+		await page.waitForSelector('#global-header');
+		const ptitle = await page.title()
+		expect(ptitle).toBe('cbTest testdmy - Home - coreBOS')
+		const h1Handle = await page.$('button[title="CRM Settings"]');
+		expect(h1Handle).toBeNull() // it doesn't exist
+		browser.close()
+	} else {
+		throw 'URL could not be determined from config file'
+	}
+})
+
+test('Inactive user login', async () => {
+	jest.setTimeout(30000);
+	const url = getUrlFromConfigFile();
+	if (!!url) {
+		// Uncomment this section to actually see the test happening
+		const browser = await puppeteer.launch({
+			// headless: false,
+			// slowMo: 80,
+			// args: ['--window-size=1920,1080'],
+			defaultViewport: null
+		})
+
+		const page = await browser.newPage()
+		await page.goto(url)
+		await page.type('input#username', 'testinactive')
+		await page.type('input#password', 'testinactive')
+		await page.click('input#Login')
+		await page.waitForSelector('div.errorMessage');
+		const divcnt = await page.$eval('div.errorMessage', el => el.innerHTML)
+		expect(divcnt).toBe('You must specify a valid username and password.')
+	} else {
+		throw 'URL could not be determined from config file'
+	}
+})
