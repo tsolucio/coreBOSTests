d3f585c803ac342d3745849abe412b4105d1e161
joebordes <joe@tsolucio.com>
2020-03-21 00:02:13 +0100

    (WS:vtws_getReferenceValue) test user fields

--- include/Webservices/getReferenceValueTest.php
+++ include/Webservices/getReferenceValueTest.php
@@ -33,7 +33,7 @@ class testgetReferenceValue extends TestCase {
 		$actual = vtws_getReferenceValue(serialize(array('11x74','12x1085','20x2','21x1','22x2')), $current_user);
 		$expected = 'a:5:{s:5:"11x74";a:3:{s:6:"module";s:8:"Accounts";s:9:"reference";s:15:"Chemex Labs Ltd";s:6:"cbuuid";s:40:"b0857db0c1dee95300a10982853f5fb1d4e981c1";}s:7:"12x1085";a:3:{s:6:"module";s:8:"Contacts";s:9:"reference";s:15:"Julieta Cropsey";s:6:"cbuuid";s:40:"d9692723137a4f119c900157b38189738e692fc4";}s:4:"20x2";a:3:{s:6:"module";s:6:"Groups";s:9:"reference";s:12:"Team Selling";s:6:"cbuuid";s:0:"";}s:4:"21x1";a:3:{s:6:"module";s:8:"Currency";s:9:"reference";s:13:"Euro : &euro;";s:6:"cbuuid";s:0:"";}s:4:"22x2";a:3:{s:6:"module";s:15:"DocumentFolders";s:9:"reference";s:8:"Avengers";s:6:"cbuuid";s:0:"";}}';
 		$this->assertEquals($expected, $actual, "testvtws_getReferenceValue");
-		$actual = vtws_getReferenceValue(serialize(array('12x1084|11x74')), $current_user);
+		$actual = vtws_getReferenceValue(serialize(array('12x1084|11x74', '19x1', '19x7')), $current_user);
 		$expected = serialize(array(
 			'12x1084' => array(
 				'module' => 'Contacts',
@@ -44,6 +44,16 @@ class testgetReferenceValue extends TestCase {
 				'module' => 'Accounts',
 				'reference' => 'Chemex Labs Ltd',
 				'cbuuid' => 'b0857db0c1dee95300a10982853f5fb1d4e981c1',
+			),
+			'19x1' => array(
+				'module' => 'Users',
+				'reference' => ' Administrator',
+				'cbuuid' => '',
+			),
+			'19x7' => array(
+				'module' => 'Users',
+				'reference' => 'cbTest testymd',
+				'cbuuid' => '',
 			),
 		));
 		$this->assertEquals($expected, $actual, "testvtws_getReferenceValue");
