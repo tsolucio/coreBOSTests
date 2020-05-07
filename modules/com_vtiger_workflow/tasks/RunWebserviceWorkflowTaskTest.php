57a59fcd3d3c9c8e6be6a9040a058694d14c5130
joebordes <joe@tsolucio.com>
2020-05-07 18:22:12 +0200

    (Workflow) run web service task

---
+++ modules/com_vtiger_workflow/tasks/RunWebserviceWorkflowTaskTest.php
@@ -0,0 +1,50 @@
+<?php
+/*************************************************************************************************
+ * Copyright 2020 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Tests.
+ * The MIT License (MIT)
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *************************************************************************************************/
+use PHPUnit\Framework\TestCase;
+
+include_once 'modules/com_vtiger_workflow/tasks/RunWebserviceWorkflowTask.inc';
+
+class RunWebserviceWorkflowTaskTest extends TestCase {
+
+	/**
+	 * Method substituteSettingProvidor
+	 * params
+	 */
+	public function substituteSettingProvidor() {
+		return array(
+			array('http://url.tld/1', 'http://url.tld/1'),
+			array('getSetting(cbodLastLoginTime1)', '1588852622'),
+			array('thingsinfrontgetSetting(cbodLastLoginTime1)', 'thingsinfront1588852622'),
+			array('getSetting(cbodLastLoginTime1)thingsinback', '1588852622thingsinback'),
+			array('thingsinfrontgetSetting(cbodLastLoginTime1)thingsinback', 'thingsinfront1588852622thingsinback'),
+			array('thi(ng)sinfrontgetSetting(cbodLastLoginTime1)thing(si)nback', 'thi(ng)sinfront1588852622thing(si)nback'),
+		);
+	}
+
+	/**
+	 * Method testsubstituteSetting
+	 * @test
+	 * @dataProvider substituteSettingProvidor
+	 */
+	public function testsubstituteSetting($url, $expected) {
+		$rwftsk = new RunWebserviceWorkflowTask();
+		$this->assertEquals($expected, $rwftsk->substituteSetting($url));
+	}
+}
