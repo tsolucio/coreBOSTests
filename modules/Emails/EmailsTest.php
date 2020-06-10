bd3e7419f39894fbb5f632445f2f157a1cc5919a
joebordes <joe@tsolucio.com>
2020-06-10 20:26:25 +0200

    (Emails) start

---
+++ modules/Emails/EmailsTest.php
@@ -0,0 +1,34 @@
+<?php
+/*************************************************************************************************
+ * Copyright 2020 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Tests.
+ * The MIT License (MIT)
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *************************************************************************************************/
+include_once 'modules/Emails/Emails.php';
+use PHPUnit\Framework\TestCase;
+
+class EmailsTest extends TestCase {
+
+	/**
+	 * Method testEmails
+	 * @test
+	 */
+	public function testEmails() {
+		$Emails = new Emails();
+		$this->assertEquals('Emails', get_class($Emails), 'Class instantiated correctly');
+		$this->assertEquals('vtiger_activity', $Emails->table_name, 'is email module');
+	}
+}
