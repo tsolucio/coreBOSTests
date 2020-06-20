327bd2ffff3ef63c81a4ef930ddff90b100d368d
joebordes <joe@tsolucio.com>
2020-06-20 20:43:50 +0200

    (Webservice) challenge and login

---
+++ include/Webservices/AuthTokenTest.php
@@ -0,0 +1,78 @@
+<?php
+/*************************************************************************************************
+ * Copyright 2020 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Tests.
+ * The MIT License (MIT)
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *************************************************************************************************/
+
+use PHPUnit\Framework\TestCase;
+
+include_once 'include/Webservices/AuthToken.php';
+
+class testWSAuthToken extends TestCase {
+
+	/**
+	 * Method testchallengewrong
+	 * @test
+	 * @expectedException WebServiceException
+	 */
+	public function testchallengewrong() {
+		vtws_getchallenge('user_does_not_exist');
+	}
+
+	/**
+	 * Method testchallengeempty
+	 * @test
+	 * @expectedException WebServiceException
+	 */
+	public function testchallengeempty() {
+		vtws_getchallenge('');
+	}
+
+	/**
+	 * Method testvtws_getchallenge
+	 * @test
+	 */
+	public function testvtws_getchallenge() {
+		global $adb;
+		$adb->query('DELETE FROM vtiger_ws_userauthtoken WHERE userid=1');
+		$actual = vtws_getchallenge('admin');
+		$this->assertEquals(array('token', 'serverTime', 'expireTime'), array_keys($actual));
+		$get_token = $adb->pquery('SELECT * FROM vtiger_ws_userauthtoken WHERE userid=?', array(1));
+		$this->assertEquals($get_token->fields['token'], $actual['token']);
+		$this->assertEquals($get_token->fields['expiretime'], $actual['expireTime']);
+		$actualupd = vtws_getchallenge('admin');
+		$this->assertEquals(array('token', 'serverTime', 'expireTime'), array_keys($actualupd));
+		$get_token = $adb->pquery('SELECT * FROM vtiger_ws_userauthtoken WHERE userid=?', array(1));
+		$this->assertEquals($get_token->fields['token'], $actualupd['token']);
+		$this->assertEquals($get_token->fields['expiretime'], $actualupd['expireTime']);
+		$this->assertEquals($actual['token'], $actualupd['token']);
+		$this->assertEquals($actual['expireTime'], $actualupd['expireTime']);
+		$this->assertEquals($actual['serverTime'], $actualupd['serverTime']);
+		sleep(1);
+		$new_expire_time = time()-(60*120); // simulate passing of time
+		$adb->pquery('UPDATE vtiger_ws_userauthtoken SET expiretime=? WHERE userid=?', array($new_expire_time, 1));
+		$actualupd = vtws_getchallenge('admin');
+		$this->assertEquals(array('token', 'serverTime', 'expireTime'), array_keys($actualupd));
+		$get_token = $adb->pquery('SELECT * FROM vtiger_ws_userauthtoken WHERE userid=?', array(1));
+		$this->assertEquals($get_token->fields['token'], $actualupd['token']);
+		$this->assertEquals($get_token->fields['expiretime'], $actualupd['expireTime']);
+		$this->assertNotEquals($actual['token'], $actualupd['token']);
+		$this->assertNotEquals($actual['expireTime'], $actualupd['expireTime']);
+		$this->assertNotEquals($actual['serverTime'], $actualupd['serverTime']);
+	}
+}
+?>
---
+++ include/Webservices/LoginTest.php
@@ -0,0 +1,122 @@
+<?php
+/*************************************************************************************************
+ * Copyright 2020 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Tests.
+ * The MIT License (MIT)
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *************************************************************************************************/
+
+use PHPUnit\Framework\TestCase;
+
+include_once 'include/Webservices/AuthToken.php';
+include_once 'include/Webservices/Login.php';
+require_once 'include/Webservices/WebServiceErrorCode.php';
+
+class testWSLogin extends TestCase {
+
+	/**
+	 * Method testwronguser
+	 * @test
+	 * @expectedException WebServiceException
+	 */
+	public function testwronguser() {
+		$this->expectException(WebServiceException::class);
+		$this->expectExceptionCode(WebServiceErrorCode::$AUTHREQUIRED);
+		vtws_login('user_does_not_exist', 'password');
+	}
+
+	/**
+	 * Method testnotoken
+	 * @test
+	 * @expectedException WebServiceException
+	 */
+	public function testnotoken() {
+		global $adb;
+		$adb->query('DELETE FROM vtiger_ws_userauthtoken WHERE userid=1');
+		$this->expectException(WebServiceException::class);
+		$this->expectExceptionCode(WebServiceErrorCode::$INVALIDTOKEN);
+		vtws_login('admin', 'no token');
+	}
+
+	/**
+	 * Method testwrongtoken
+	 * @test
+	 * @expectedException WebServiceException
+	 */
+	public function testwrongtoken() {
+		vtws_getchallenge('admin');
+		$this->expectException(WebServiceException::class);
+		$this->expectExceptionCode(WebServiceErrorCode::$INVALIDUSERPWD);
+		vtws_login('admin', 'invalid token');
+	}
+
+	/**
+	 * Method testnokey
+	 * @test
+	 * @expectedException WebServiceException
+	 */
+	public function testnokey() {
+		global $adb;
+		$usr = $adb->query('Select accesskey FROM vtiger_users WHERE id=9');
+		$accesskey = $usr->fields['accesskey'];
+		$adb->query('update vtiger_users set accesskey="" WHERE id=9');
+		$this->expectException(WebServiceException::class);
+		$this->expectExceptionCode(WebServiceErrorCode::$ACCESSKEYUNDEFINED);
+		try {
+			vtws_login('testinactive', 'invalid token');
+		} catch (\Throwable $th) {
+			$adb->pquery('update vtiger_users set accesskey=? WHERE id=9', array($accesskey));
+			throw $th;
+		}
+		$adb->pquery('update vtiger_users set accesskey=? WHERE id=9', array($accesskey));
+	}
+
+	/**
+	 * Method testinactiveuser
+	 * @test
+	 * @expectedException WebServiceException
+	 */
+	public function testinactiveuser() {
+		$token = vtws_getchallenge('testinactive');
+		$this->expectException(WebServiceException::class);
+		$this->expectExceptionCode(WebServiceErrorCode::$AUTHREQUIRED);
+		vtws_login('testinactive', md5($token['token'].'1AfgXxwVqMKSKGBM'));
+	}
+
+	/**
+	 * Method testloginaccesskey
+	 * @test
+	 */
+	public function testloginaccesskey() {
+		$token = vtws_getchallenge('admin');
+		$actual = vtws_login('admin', md5($token['token'].'cdYTBpiMR9RfGgO'));
+		$expected = new Users();
+		$expected->retrieveCurrentUserInfoFromFile(1);
+		$this->assertEquals($expected, $actual);
+	}
+
+	/**
+	 * Method testloginpassword
+	 * @test
+	 */
+	public function testloginpassword() {
+		$token = vtws_getchallenge('admin');
+		$actual = vtws_login('admin', $token['token'].'admin');
+		$expected = new Users();
+		$expected->retrieveCurrentUserInfoFromFile(1);
+		$this->assertEquals($expected, $actual);
+	}
+}
+?>
