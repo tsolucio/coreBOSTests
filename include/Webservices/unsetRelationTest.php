c6a3bb034d21142a4154dddcb74da52ab63d45dc
joebordes <joe@tsolucio.com>
2020-06-28 13:33:15 +0200

    (WS:set/unset Relations)

--- include/Webservices/unsetRelationTest.php
+++ include/Webservices/unsetRelationTest.php
@@ -20,46 +20,18 @@
 
 use PHPUnit\Framework\TestCase;
 
-include_once 'include/Webservices/SetRelation.php';
 include_once 'include/Webservices/UnsetRelation.php';
 
-class testSetRelation extends TestCase {
+class testUnsetRelation extends TestCase {
 
 	public $usrcoma3dot = 10; // testtz
 
 	/**
-	 * Method testvtws_setrelation
+	 * Method testvtws_unsetrelation
 	 * @test
 	 */
-	public function testvtws_setrelation() {
-		global $current_user, $adb;
-		$relateThisId = '1x4817'; // Campaign
-		$withTheseIds = array('11x82','11x83','12x1110','12x1111');
-		$rs = $adb->query('SELECT count(*) as cnt from vtiger_campaignaccountrel where accountid in (82,83) AND campaignid=4817');
-		$this->assertEquals(0, $rs->fields['cnt']);
-		$rs = $adb->query('SELECT count(*) as cnt from vtiger_campaigncontrel where contactid in (1110,1111) AND campaignid=4817');
-		$this->assertEquals(0, $rs->fields['cnt']);
-		$this->assertTrue(vtws_setrelation($relateThisId, $withTheseIds, $current_user));
-		$rs = $adb->query('SELECT count(*) as cnt from vtiger_campaignaccountrel where accountid in (82,83) AND campaignid=4817');
-		$this->assertEquals(2, $rs->fields['cnt']);
-		$rs = $adb->query('SELECT count(*) as cnt from vtiger_campaigncontrel where contactid in (1110,1111) AND campaignid=4817');
-		$this->assertEquals(2, $rs->fields['cnt']);
-		$this->assertTrue(vtws_unsetrelation($relateThisId, $withTheseIds, $current_user));
-		$rs = $adb->query('SELECT count(*) as cnt from vtiger_campaignaccountrel where accountid in (82,83) AND campaignid=4817');
-		$this->assertEquals(0, $rs->fields['cnt']);
-		$rs = $adb->query('SELECT count(*) as cnt from vtiger_campaigncontrel where contactid in (1110,1111) AND campaignid=4817');
-		$this->assertEquals(0, $rs->fields['cnt']);
-		//////// cbuuid
-		$relateThisId = '16e91c4d141043693fea5eeecf63693fe550894f'; // HelpDesk
-		$withTheseIds = array('d66fbee49b3a3f4e81e326918d5b0e6f348f02fa'); // Service Contract
-		$rs = $adb->query('SELECT count(*) as cnt from vtiger_crmentityrel where (crmid=2741 AND relcrmid=9782) or (crmid=9782 AND relcrmid=2741)');
-		$this->assertEquals(0, $rs->fields['cnt']);
-		$this->assertTrue(vtws_setrelation($relateThisId, $withTheseIds, $current_user));
-		$rs = $adb->query('SELECT count(*) as cnt from vtiger_crmentityrel where (crmid=2741 AND relcrmid=9782) or (crmid=9782 AND relcrmid=2741)');
-		$this->assertEquals(1, $rs->fields['cnt']);
-		$this->assertTrue(vtws_unsetrelation($relateThisId, $withTheseIds, $current_user));
-		$rs = $adb->query('SELECT count(*) as cnt from vtiger_crmentityrel where (crmid=2741 AND relcrmid=9782) or (crmid=9782 AND relcrmid=2741)');
-		$this->assertEquals(0, $rs->fields['cnt']);
+	public function testvtws_unsetrelation() {
+		$this->assertTrue(true, 'this is tested in setrelation');
 	}
 
 	/**
@@ -77,7 +49,7 @@ class testSetRelation extends TestCase {
 		$this->expectException(WebServiceException::class);
 		$this->expectExceptionCode(WebServiceErrorCode::$ACCESSDENIED);
 		try {
-			vtws_setrelation($pdoID.'x27153', array(), $user);
+			vtws_unsetrelation($pdoID.'x27153', array(), $user);
 		} catch (\Throwable $th) {
 			$current_user = $holduser;
 			throw $th;
@@ -94,7 +66,7 @@ class testSetRelation extends TestCase {
 		$this->expectException(WebServiceException::class);
 		$this->expectExceptionCode(WebServiceErrorCode::$INVALIDID);
 		// 12192 is a quote
-		vtws_setrelation(vtws_getEntityId('SalesOrder').'x12192', array(), $current_user);
+		vtws_unsetrelation(vtws_getEntityId('SalesOrder').'x12192', array(), $current_user);
 	}
 
 	/**
@@ -112,7 +84,7 @@ class testSetRelation extends TestCase {
 		$this->expectException(WebServiceException::class);
 		$this->expectExceptionCode(WebServiceErrorCode::$ACCESSDENIED);
 		try {
-			vtws_setrelation($pdoID.'x2633', array(), $user);
+			vtws_unsetrelation($pdoID.'x2633', array(), $user);
 		} catch (\Throwable $th) {
 			$current_user = $holduser;
 			throw $th;
