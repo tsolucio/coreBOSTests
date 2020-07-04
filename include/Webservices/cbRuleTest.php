5adad2326df557a20c7e0e4d0330a36ecee06aae
joebordes <joe@tsolucio.com>
2020-07-04 22:25:32 +0200

    (Webservice) coreBOS Rule

---
+++ include/Webservices/cbRuleTest.php
@@ -0,0 +1,105 @@
+<?php
+/*************************************************************************************************
+ * Copyright 2020 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Tests.
+ * The MIT License (MIT)
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *************************************************************************************************/
+
+use PHPUnit\Framework\TestCase;
+
+include_once 'include/Webservices/cbRule.php';
+
+class testWScoreBOSRule extends TestCase {
+
+	/**
+	 * Method testrule
+	 * @test
+	 */
+	public function testrule() {
+		global $current_user;
+		$params = json_encode(array(
+			'record_id' => '74',
+		));
+		$actual = cbws_cbRule(vtws_getEntityId('cbMap').'x34033', $params, $current_user);
+		$this->assertEquals(141, $actual, 'cbRule');
+	}
+
+	/**
+	 * Method testinvalidmoduleexception
+	 * @test
+	 * @expectedException WebServiceException
+	 */
+	public function testinvalidmoduleexception() {
+		global $current_user;
+		$this->expectException(WebServiceException::class);
+		$this->expectExceptionCode(WebServiceErrorCode::$ACCESSDENIED);
+		cbws_cbRule('11x74', array(), $current_user);
+	}
+
+	/**
+	 * Method testReadExceptionNoPermission
+	 * @test
+	 * @expectedException WebServiceException
+	 */
+	public function testReadExceptionNoPermission() {
+		global $current_user;
+		$holduser = $current_user;
+		$user = new Users();
+		$user->retrieveCurrentUserInfoFromFile(5); // testdmy
+		$current_user = $user;
+		$this->expectException(WebServiceException::class);
+		$this->expectExceptionCode(WebServiceErrorCode::$ACCESSDENIED);
+		try {
+			cbws_cbRule(vtws_getEntityId('cbMap').'x34030', array(), $current_user);
+		} catch (\Throwable $th) {
+			$current_user = $holduser;
+			throw $th;
+		}
+	}
+
+	/**
+	 * Method testInvalidContextException
+	 * @test
+	 * @expectedException WebServiceException
+	 */
+	public function testInvalidContextException() {
+		global $current_user;
+		$this->expectException(WebServiceException::class);
+		$this->expectExceptionCode(WebServiceErrorCode::$INVALIDID);
+		try {
+			cbws_cbRule(vtws_getEntityId('cbMap').'x34033', '{incorrect JSON', $current_user);
+		} catch (\Throwable $th) {
+			throw $th;
+		}
+	}
+
+	/**
+	 * Method testInvalidIDExceptionMissingrecord
+	 * @test
+	 * @expectedException WebServiceException
+	 */
+	public function testInvalidIDExceptionMissingrecord() {
+		global $current_user;
+		$this->expectException(WebServiceException::class);
+		$this->expectExceptionCode(WebServiceErrorCode::$INVALIDID);
+		try {
+			cbws_cbRule(vtws_getEntityId('cbMap').'x34033', '{}', $current_user);
+		} catch (\Throwable $th) {
+			throw $th;
+		}
+	}
+}
+?>
