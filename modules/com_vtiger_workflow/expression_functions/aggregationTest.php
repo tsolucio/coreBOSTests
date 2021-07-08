e9b6f13c7797eba1ca80b450a04e047e9022dc26
joebordes <joe@tsolucio.com>
2021-07-08 18:08:45 +0200

    (workflow) aggregation group_concat with 'name'

--- modules/com_vtiger_workflow/expression_functions/aggregationTest.php
+++ modules/com_vtiger_workflow/expression_functions/aggregationTest.php
@@ -101,6 +101,8 @@ class workflowfunctionsaggregationTest extends TestCase {
 		$this->assertEquals('Sean Cassidy,Garth (Other Realm)', $actual);
 		$actual = __cb_aggregation(array('group_concat','Invoice','subject separator ";"','',$entityData));
 		$this->assertEquals('Sean Cassidy;Garth (Other Realm)', $actual);
+		$actual = __cb_aggregation(array('group_concat','Invoice','account_id separator ";"name','',$entityData));
+		$this->assertEquals('Chemex Labs Ltd;Newton Clerk', $actual);
 		////////////////
 		$currentModule = 'Potentials';
 		$entityData = $entityCache->forId('13x5900'); // Egestas Aliquam Fringilla Corp potential
